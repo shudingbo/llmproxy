6fd131f14b4877af20df60dc54ec1853f290f4e8
shudingbo <shudingbo@qq.com>
2026-08-03 17:07:13 +0800

    fix(web): 拦截 Edge 最小化时 history.replaceState 异常
    Edge 浏览器最小化恢复时 vue-router 可能异常调用 replaceState，页面隐藏状态下直接忽略

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,6 +7,25 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
+
+/**************** 拦截 history 方法 ，处理 vue router 在 edge 浏览器 最小化时，会恢复*/
+// 检测是否为 Edge 浏览器
+const isEdge = /Edg\//.test(navigator.userAgent);
+if (isEdge) {
+  // 保存原生 replaceState 方法
+  const originalReplaceState = history.replaceState;
+
+  // 用新方法替换它
+  history.replaceState = function (...args) {
+    // 如果当前页面是隐藏状态，直接返回，不执行 replaceState
+    if (document.visibilityState === "hidden") {
+      return;
+    }
+    // 否则，调用原生的 replaceState 方法
+    return originalReplaceState.apply(this, args);
+  };
+}
+
 // 创建应用实例
 const app = createApp(App)
 
